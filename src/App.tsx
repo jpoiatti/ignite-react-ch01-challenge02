467945c65edf2f953fa32f17a858f9da494f4a98
Jessé Poiatti <j.poiatti@gmail.com>
2022-05-18 18:51:04 -0300

    Update SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
+import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
 
-import './styles/sidebar.scss';
 import './styles/content.scss';
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
 
 interface MovieProps {
   imdbID: string;
@@ -60,26 +52,14 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <SideBar 
+        genres ={genres} 
+        selectedGenreId={selectedGenreId} 
+        handleClickButton={handleClickButton} />
 
       <div className="container">
         <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+          <span className="category">Categoria: <span>{selectedGenre.title}</span></span>
         </header>
 
         <main>
